f3616eb69dc21e419d0deece8d3f09b166e4b89c
Your Name <muhammadgamer342@gmail.com>
2024-06-18 06:21:14 -0700

    first commit

---
+++ app/components/coming-soon.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Image from 'next/image'
+
+function comingsoon() {
+  return (
+    <main>
+      <br />
+    <div className='cursor-pointer text-5xl font-mono text-center mt-56'>Coming Soon</div>
+    <br />
+    <br />
+    <p className='cursor-pointer text-2xl font-medium text-center '>MBZ Real Estate Website is <span className='text-lime-500'>Under Construction</span>.</p>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <p className='cursor-pointer text-center'>&copy; {new Date().getFullYear()} MBZ Real Estate</p>
+
+    </main>
+  )
+}
+
+export default comingsoon
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import ComingSoon from "./components/coming-soon";
+
+
+export default function Home() {
+  return (
+   <div>
+    <ComingSoon />
+   </div>
+  )
+}
